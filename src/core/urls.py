6feb985b6dc359a6877be46069a04f2f069ce92e
angelo-francisco <ics20080729@gmail.com>
2024-07-30 20:14:27 +0100

    new handler location seted on core urls.py

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("weather.urls"))]
 
-handler404 = "weather.views.handler404"
-handler500 = "weather.views.handler500"
+handler404 = "weather.handlers.handler404"
+handler500 = "weather.handlers.handler500"
