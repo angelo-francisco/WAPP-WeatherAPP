317fb65b873fce6e4bebe1b9ed9c37c09766e6a1
angelo-francisco <ics20080729@gmail.com>
2024-07-30 18:56:35 +0100

    project core modified to name core

--- src/core/settings.py
+++ src/core/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "WeatherApp.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -56,7 +56,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "WeatherApp.wsgi.application"
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
