f7fe61d5740b45e75b57b1c534f1ed7480f74c74
angelo-francisco <ics20080729@gmail.com>
2024-07-30 18:57:33 +0100

    Weather renamed to weather

--- src/Weather/apps.py
+++ src/Weather/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WeatherConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Weather"
+    name = "weather"
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("Weather.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("weather.urls"))]
 
-handler404 = "Weather.views.handler404"
-handler500 = "Weather.views.handler500"
+handler404 = "weather.views.handler404"
+handler500 = "weather.views.handler500"
