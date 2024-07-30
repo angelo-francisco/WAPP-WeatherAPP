bc43b5abbb57b357b29ee1810c4652f28a84e148
angelo-francisco <ics20080729@gmail.com>
2024-07-30 19:31:55 +0100

    static confs removed

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "weather",
+    "weather"
 ]
 
 MIDDLEWARE = [
@@ -99,14 +99,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR / "staticfiles")]
-STATIC_ROOT = os.path.join(BASE_DIR / "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
